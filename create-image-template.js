3633eacd4ec51d48103a92e0ad1b89478a890fc0
Peter Job <peter.job@d55.co.uk>
2021-07-06 18:15:40 -0600

    cleanup

--- create-image-template.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const path = 'img/favicons';
-
-let files = fs.readdirSync(path);
-let template = files.map(file => `<img src="${path}/${file}" width="24"></img>`);
-fs.writeFileSync('template.txt', template.join('\n'));
